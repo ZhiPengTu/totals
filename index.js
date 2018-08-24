9276cfef1846938264d9f2bb698a3ea2934c31e2
ZhiPengTu <17600380959@163.com>
2018-08-25 00:31:51 +0800

    v 1

--- index.js
+++ index.js
@@ -5,5 +5,33 @@
 const hello = function(key){
     console.log('Hello World!');
 };
+var layUI=layUI||{};
+    function totals(options){
+        this.timer=null;
+        this.options={
+            delay:3000,
+            message:'hello layUI!'
+        }
+        $.extend(this.options, options);
+        this.init();
+    }
+    totals.prototype.constructor=totals;
+    totals.prototype.init=function(){
+        var _this = this;
+        var totalswrap=document.createElement('div');
+        totalswrap.className='layui-totals';
+        var totalsSpan=document.createElement('span');
+        totalsSpan.innerText=_this.options.message;
+        totalswrap.appendChild(totalsSpan);
+        var body=document.getElementsByTagName('body')[0];
+        body.appendChild(totalswrap);
+        setTimeout(function(){
+            body.removeChild(totalswrap);
+        },_this.options.delay);
+    };
+    
+    layUI.totals=function(options){
+        new totals(options);
+    }
 
 exports.hello     = hello;
