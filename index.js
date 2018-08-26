f3fe0b473a3d142665251ea5eddf945659557302
ZhiPengTu <17600380959@163.com>
2018-08-26 20:42:25 +0800

    version 1.0.4  修改export 方式

--- index.js
+++ index.js
@@ -2,33 +2,36 @@
 *  _totals
 * @function _totals
 **/ 
-    const $ =require('jquery');   
-    const _totals=function(options){
-        this.timer=null;
-        this.options={
-            delay:3000,
-            message:'hello layUI!'
-        }
-       $.extend(this.options, options);
-        this.init();
+const $ =require('jquery');   
+const _totals=function(options){
+    this.timer=null;
+    this.options={
+        delay:3000,
+        message:'hello layUI!'
     }
-    _totals.prototype.constructor=totals;
-    _totals.prototype.init=function(){ß
-        var _this = this;
-        var totalswrap=document.createElement('div');
-        totalswrap.className='layui-totals';
-        var totalsSpan=document.createElement('span');
-        totalsSpan.innerText=_this.options.message;
-        totalswrap.appendChild(totalsSpan);
-        var body=document.getElementsByTagName('body')[0];
-        body.appendChild(totalswrap);
-        setTimeout(function(){
-            body.removeChild(totalswrap);
-        },_this.options.delay);
-    };
-    
-    let totals = function(options){
-       this.totals = new _totals(options);
-    }
+   $.extend(this.options, options);
+    this.init();
+}
+_totals.prototype.constructor=_totals;
+_totals.prototype.init=function(){
+    var _this = this;
+    var totalswrap=document.createElement('div');
+    totalswrap.className='layui-totals';
+    var totalsSpan=document.createElement('span');
+    totalsSpan.innerText=_this.options.message;
+    totalswrap.appendChild(totalsSpan);
+    var body=document.getElementsByTagName('body')[0];
+    body.appendChild(totalswrap);
+    setTimeout(function(){
+        body.removeChild(totalswrap);
+    },_this.options.delay);
+};
 
-exports.totals=totals;
+const totals = function(options){
+   return new _totals(options);
+}
+
+module.exports = {  
+    totals: totals
+}
+
